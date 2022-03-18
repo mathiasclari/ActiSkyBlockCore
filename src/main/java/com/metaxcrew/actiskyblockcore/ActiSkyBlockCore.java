c2a94900b299a57807a4b9cb62b7cf5f89106866
Mat <silencekrakens@gmail.com>
2022-03-18 09:22:05 +0100

    Initial commit

---
+++ src/main/java/com/metaxcrew/actiskyblockcore/ActiSkyBlockCore.java
@@ -0,0 +1,17 @@
+package com.metaxcrew.actiskyblockcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ActiSkyBlockCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
