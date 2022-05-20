8fa795e5e1228109c4bd884bde88bf7dafab37fb
Mat <silencekrakens@gmail.com>
2022-05-20 18:38:23 +0200

    Updated HatCommand

--- src/main/java/com/metaxcrew/actiskyblockcore/ultimatecommands/HatCommand.java
+++ src/main/java/com/metaxcrew/actiskyblockcore/ultimatecommands/HatCommand.java
@@ -25,7 +25,7 @@ public class HatCommand implements CommandExecutor {
                         p.getInventory().setHelmet(new ItemStack(p.getInventory().getItemInMainHand()));
                         p.getInventory().removeItem(new ItemStack(p.getInventory().getItemInMainHand()));
                         p.playEffect(p.getLocation(), Effect.CLICK1, 5);
-                        p.playEffect(p.getLocation(), Effect.BOW_FIRE, 5);
+                        p.playEffect(p.getLocation(), Effect.BOW_FIRE, 6);
                         sender.sendMessage(ChatColor.of("#94A8E2") + "Hat has been set!");
                         return true;
                     }
@@ -35,5 +35,8 @@ public class HatCommand implements CommandExecutor {
                 }
         }
         return true;
+
+        //ME: is this plugin going to work now?
+        //AI:
     }
 }
